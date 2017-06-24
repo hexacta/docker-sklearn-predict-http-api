92de657717ea20250d2475234c3beaf772d7152e
pomber <pombopombopombo@gmail.com>
2017-06-24 14:20:47 -0300

    Add error description to response

--- server.py
+++ server.py
@@ -9,32 +9,44 @@ model = joblib.load('model.pkl')
 @route('/predict')
 def predict():
     response.content_type = 'application/json'
-    body = request.json
-    body = body if isinstance(body, list) else [body]
-    X = pandas.DataFrame.from_dict(body)
-    result = model.predict(X)
-    return json.dumps(result.tolist())
+    try:
+        body = request.json
+        body = body if isinstance(body, list) else [body]
+        X = pandas.DataFrame.from_dict(body)
+        result = model.predict(X)
+        return json.dumps(result.tolist())
+    except Exception as error:
+        response.status = 500
+        return json.dumps({'error': str(error)})
 
 
 @route('/predictproba')
 def predictproba():
     response.content_type = 'application/json'
-    body = request.json
-    body = body if isinstance(body, list) else [body]
-    X = pandas.DataFrame.from_dict(body)
-    result = pandas.DataFrame(model.predict_proba(X),
-                              columns=model.classes_)
-    return result.to_json(orient="records")
+    try:
+        body = request.json
+        body = body if isinstance(body, list) else [body]
+        X = pandas.DataFrame.from_dict(body)
+        result = pandas.DataFrame(model.predict_proba(X),
+                                  columns=model.classes_)
+        return result.to_json(orient="records")
+    except Exception as error:
+        response.status = 500
+        return json.dumps({'error': str(error)})
 
 
 @route('/featurenames')
 def featurenames():
     response.content_type = 'application/json'
-    result = model.get_booster().feature_names
-    return json.dumps(result)
+    try:
+        result = model.get_booster().feature_names
+        return json.dumps(result)
+    except Exception as error:
+        response.status = 500
+        return json.dumps({'error': str(error)})
 
 
 #run(host='localhost', port=8080, debug=True, reloader=True)
 run(host='0.0.0.0', port=8080)
 
-# curl -H "Content-Type: application/json" -X GET -d '[{"Age":1},{"Age":25}]' http://localhost:8080/predictproba
+# curl -H "Content-Type: application/json" -X GET -d '[{"sepal length (cm)":4.4}]' http://localhost:8080/predictproba
